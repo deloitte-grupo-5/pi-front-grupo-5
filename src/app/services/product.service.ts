f3f3fe66dc8e03ba08a5057a9d69b3ec699dcb7f
FelipeALuna <felipeluna_2000@hotmail.com>
2021-10-26 15:50:02 -0300

    estilizando create contato

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -17,9 +18,9 @@ export class ProductService {
   }
   criarProduto(nome:string,descricao:string,valor:number,quantidade_estoque:number){
     const url = "http://localhost:8080/produtos"
-    let token = window.sessionStorage.getItem('token');
-    console.log(token)
-    return this.http.post(url,{descricao,nome,quantidade_estoque,valor},{headers:{Authorization:`${token}`}})
+    let token = window.sessionStorage.getItem("token");
+    let teste =token!.replace(/"([^"]+(?="))"/g, '$1');
+    return this.http.post(url,{descricao,nome,quantidade_estoque,valor},{headers:{Authorization:teste}})
   }
 
 
