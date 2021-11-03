9881ecde3d5ba4b8d81e15bc9b652be84712bd0a
Bruna Salom <bruna.salom@gmail.com>
2021-11-03 20:19:32 -0300

    puxando img do banco com url e ajeitando o css pra quando elas vierem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
     path:"sobre", component:AboutUsComponent
   },{
     path:"perfil", 
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     component:PerfilComponent
   },{
     path:"teste", component:ProductDetailComponent
