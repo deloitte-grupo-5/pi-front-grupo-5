3a4d246c31d30d68fc6991d8d1fbcc7936c0f96e
Jonathan Brant <jbrant1995@gmail.com>
2021-11-04 16:00:10 -0300

    bug fix usuario admin

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -20,7 +20,7 @@ export class ProductComponent implements OnInit {
     if(window.sessionStorage.getItem("usuario")){
       let usuario = window.sessionStorage.getItem("usuario")
       let usuarioObj = JSON.parse(usuario!);
-      if(usuarioObj.id == 1){
+      if(usuarioObj.id == 5){
         this.usuarioAdm= true;
       }
     }
