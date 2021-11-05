2c9db76c93e34a28426ebbf6b7a5cf20c16bfe8b
Bruna Salom <bruna.salom@gmail.com>
2021-11-05 10:27:56 -0300

    ajeitando css de várias coisas

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -16,61 +16,70 @@ export class SignInComponent implements OnInit {
   telefone:string=""
   senha:string=""
   usuario:string=""
-  erros:string[]= [];
+  errosNome:string[]= [];
+  errosSobrenome:string[]= [];
+  errosTelefone:string[]= [];
+  errosUsuario:string[]= [];
+  errosEmail:string[]= [];
+  errosSenha:string[]= [];
 
   constructor(private service:UserService,private router:Router) {
 
   }
   ngOnInit(): void {
   }
-
+  
   cadastrarUsuario(){
-    this.erros=[]
+    this.errosNome = [];
+    this.errosSobrenome = [];
+    this.errosTelefone = [];
+    this.errosUsuario = [];
+    this.errosEmail = [];
+    this.errosSenha = [];
     let validacao = true;
+
     if(this.nome == ""){
       validacao = false
-      this.erros.push("Preencha o campo Nome")
-    }
-
-    if(this.email==""){
-      this.erros.push("Preencha o campo Email")
-    }
-    if(this.sobrenome == ""){
-      validacao = false
-      this.erros.push("Preencha o campo Sobrenome")
-    }
-
-    if(this.usuario==""){
-      validacao = false
-      this.erros.push("Preencha o campo Usuário")
-    }
-    if(this.senha==""){
-      validacao = false
-      this.erros.push("Preencha o campo Senha")
-    }
-    if(this.usuario.length<5){
-      validacao = false
-      this.erros.push("O seu usuário deve ter no minimo 5 caracteres")
-    }
-    if(this.usuario.length<5){
-      validacao = false
-      this.erros.push("Sua senha deve ter no minimo 5 caracteres")
+      this.errosNome.push("• Preencha o campo Nome")
     }
     if(this.nome.length>100){
       validacao = false
-      this.erros.push("O seu nome deve conter no máximo 100 cacteres")
+      this.errosNome.push("• Máximo 100 cacteres")
+    }
+    if(this.sobrenome == ""){
+      validacao = false
+      this.errosSobrenome.push("• Preencha o campo Sobrenome")
+    }
+    if(this.usuario==""){
+      validacao = false
+      this.errosUsuario.push("• Preencha o campo Usuário")
+    }
+    if(this.usuario != "" && this.usuario.length<5){
+      validacao = false
+      this.errosUsuario.push("• Mínimo 5 caracteres")
     }
     if(this.usuario.length>100){
       validacao = false
-      this.erros.push("O seu usuário deve conter no máximo 100 cacteres")
+      this.errosUsuario.push("• Máximo 100 cacteres")
     }
-    if(this.senha.length>100){
-      validacao = false
-      this.erros.push("A sua senha deve conter no máximo 100 cacteres")
+    if(this.email==""){
+      this.errosEmail.push("• Preencha o campo Email")
     }
     if(this.email.length>100){
       validacao = false
-      this.erros.push("O seu email deve conter no máximo 100 cacteres")
+      this.errosEmail.push("• Máximo 100 cacteres")
+    }
+    if(this.senha==""){
+      validacao = false
+      this.errosSenha.push("• Preencha o campo Senha")
+    }
+    if(this.senha != "" && this.senha.length<5){
+      validacao = false
+      this.errosSenha.push("• Mínimo 5 caracteres")
+    }
+    if(this.senha.length>100){
+      validacao = false
+      this.errosSenha.push("• Máximo 100 cacteres")
     }
 
 
