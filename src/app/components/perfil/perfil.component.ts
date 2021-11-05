0533ef8356619c6c018eb783fed4ce4d107c5195
raphael reis pacheco <raphael@rrpcorp.com.br>
2021-11-04 23:14:11 -0300

    cep aiusydgha

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,28 +1,42 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  styleUrls: ['./perfil.component.css'],
 })
 export class PerfilComponent implements OnInit {
-  adm = true
-  constructor(private router:Router) {
-    if(window.sessionStorage.getItem("usuario")){
-      let usuario = window.sessionStorage.getItem("usuario")
+  @Input() cep?: string;
+  cepDataResult = '';
+  resultadoLogradouro = '';
+  resultadoBairro = '';
+  resultadoLocalidade = '';
+  resultadoUf = '';
+  adm = true;
+  constructor(private router: Router) {
+    if (window.sessionStorage.getItem('usuario')) {
+      let usuario = window.sessionStorage.getItem('usuario');
       let usuarioObj = JSON.parse(usuario!);
-      if(usuarioObj.id == 5){
-        this.usuarioAdm= true;
+      if (usuarioObj.id == 5) {
+        this.usuarioAdm = true;
       }
-
-    }else{
-      this.router.navigateByUrl('/login')
+    } else {
+      this.router.navigateByUrl('/login');
     }
-
-  }
-  usuarioAdm= false
-  ngOnInit(): void {
   }
 
+  async fetchCep() {
+    console.log(this.cep);
+    const path = `https://viacep.com.br/ws/${this.cep}/json/`;
+    const response = await (await fetch(path)).json();
+
+    this.resultadoLogradouro = response.logradouro;
+    this.resultadoBairro = response.bairro;
+    this.resultadoLocalidade = response.localidade;
+    this.resultadoUf = response.uf;
+  }
+
+  usuarioAdm = false;
+  ngOnInit(): void {}
 }
