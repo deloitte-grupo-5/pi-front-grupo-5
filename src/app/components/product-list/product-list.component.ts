57a5b5af69973ea8dda81e5e1a708795978f3daa
FelipeALuna <felipeluna_2000@hotmail.com>
2021-11-07 20:30:20 -0300

    limpando campo pesquisa

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -107,6 +107,7 @@ export class ProductListComponent implements OnInit {
   limparPesquisa(){
     this.service.onProdutosMudaram.emit();
     this.atualizarProdutosExibidos();
+    this.texto =""
   }
 
 
